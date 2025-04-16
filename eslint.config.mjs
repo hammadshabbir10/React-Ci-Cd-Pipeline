0861cb31d3f8e311551ca63b4ba1fadaf973843a
hammadshabbir66 <hammadshabbir1015@gmail.com>
2025-04-17 03:25:15 +0500

    Fix: my ESLON MJS FILE

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: {
-      js,
-      react: pluginReact,
+export default {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      plugins: {
+        js,
+        react: pluginReact,
+      },
+      extends: [
+        "js/recommended",
+        "plugin:react/recommended",
+      ],
+      languageOptions: {
+        globals: globals.browser,
+      },
+      rules: {
+        // Add any specific rules you need
+      },
     },
-    extends: [
-      "js/recommended",
-      "plugin:react/recommended",
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    rules: {
-      // Add any specific rules you need
-    }
-  }
-]);
+  ],
+};
