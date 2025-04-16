8a017faca14d19bcef3ebd52ff78bde143d7d4a7
hammadshabbir66 <hammadshabbir1015@gmail.com>
2025-04-17 03:17:03 +0500

    Fix: my file Eslin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,22 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+    extends: [
+      "js/recommended",
+      "plugin:react/recommended",
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      // Add any specific rules you need
+    }
+  }
 ]);
