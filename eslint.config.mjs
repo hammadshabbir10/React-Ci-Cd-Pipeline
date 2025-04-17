0d515bfbfbf05d2c6a8b4b0473ecfd37e8bf0a46
hammadshabbir66 <hammadshabbir1015@gmail.com>
2025-04-17 20:06:55 +0500

    My All Files are Now Correct

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginJest from 'eslint-plugin-jest';
+import babelParser from '@babel/eslint-parser';
 
-export default {
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs,jsx}"],
-      plugins: {
-        js,
-        react: pluginReact,
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
       },
-      extends: [
-        "js/recommended",
-        "plugin:react/recommended",
-      ],
-      languageOptions: {
-        globals: globals.browser,
-      },
-      rules: {
-        // Add any specific rules you need
+      globals: {
+        test: 'readonly',
+        expect: 'readonly',
       },
     },
-  ],
-};
+    plugins: {
+      react: eslintPluginReact,
+      jest: eslintPluginJest,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
